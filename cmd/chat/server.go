cf7acdd84e0302aeebbb5bff3e406f624e4ea272
iggyster <iggyster@users.noreply.github.com>
2023-06-05 11:49:48 +0300

    Fix code style

--- cmd/chat/server.go
+++ cmd/chat/server.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/iggyster/lets-go-chat/internal/middleware"
-	"github.com/iggyster/lets-go-chat/internal/router"
-	"log"
-)
-
 func main() {
-	app := fiber.New()
-
-	middleware.Boot(app)
-	router.Init(app)
-
-	log.Fatal(app.Listen(":8080"))
 }
